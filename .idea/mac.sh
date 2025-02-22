5cbad9796e40bc06c075721fa2f320dc824f3237
James Thompson <jamesronaldthompson@outlook.com>
2025-02-22 09:17:56 +0000

    Converted mac.bat to mac.sh

---
+++ .idea/mac.sh
@@ -0,0 +1,10 @@
+scriptDir=$(dirname "$0"
+cd "$scriptDir"
+cd ../target
+jpackage \
+    --input "." \
+    --main-jar revisionProgram.jar \
+    --type dmg \
+    --icon "../res/mac.icns"\
+    --name "Revision Program" \
+    --mac-package-identifier com.personinexistence.revisionprogram
